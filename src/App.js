54b841eb3a7a6ffa01342f8dd5b9c09e233fc6ed
Subhojit Saha <subhojitan@gmail.com>
2022-10-19 02:23:06 +0200

    saving

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import japaneseRestaurant from './japaneseRestaurant.png';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { Header } from './components/Header';
-import { NavBar } from './components/NavBar';
-import { TypeForm } from './components/typeForm';
-import { PeopleForm } from './components/peopleForm';
-import { DateForm } from './components/dateForm';
-import { TimeForm } from './components/timeForm';
-import { Confirmation } from './components/confirmation';
-import axios from 'axios';
+import { Main } from './components/Main';
+
 
 function App() {
-  const [bookingInstructions, setBookingInstructions] = useState(null);
-  const [selectedType, setSelectedType] = useState('');
-  const [peopleAmount, setPeopleAmount] = useState(0);
-  const [selectedDate, setSelectedDate] = useState(null)
-  const [selectedTime, setSelectedTime] = useState(null)
-  const availableTypes = [
-    'Breakfast',
-    'Lunch',
-    'Dinner'
-  ]
 
   useEffect(() => {
     const unloadCallback = (event) => {
@@ -34,47 +17,15 @@ function App() {
     return () => window.removeEventListener("beforeunload", unloadCallback);
   }, []);
 
-  useEffect(() => {
-    axios.get('http://localhost:4000/api/companies/1').then(response => {
-      setBookingInstructions(response.data[0])
-    })
-    .catch(err => console.log(err))
-  }, []);
 
-  console.log(bookingInstructions)
-  if(bookingInstructions) {
-    return (
-      <Router>
-        <Header RestaurantName={bookingInstructions.company_name} />
-        <main style={{display: 'flex', width: '100%', height: '100%'}}>
-          <div className='restaurantImage' style={{ padding: '50px 0px 50px 80px'}}>
-            <img src={japaneseRestaurant} width={450} height={600} alt='Restaurant' />
-          </div>
-          <div className='bookingForm' style={{width: '100%', textAlign: 'center'}}>
-            <NavBar 
-              selectedType={selectedType}
-              peopleAmount={peopleAmount}
-              selectedDate={selectedDate}
-              selectedTime={selectedTime}
-            />
-            <Routes>
-              <Route path='/book/:restaurantId' element={<TypeForm AvailableTypes={availableTypes} setSelectedType={setSelectedType} />} />
-              <Route path='/people' element={<PeopleForm peopleAmount={peopleAmount} setPeopleAmount={setPeopleAmount} contactPersonEmail={bookingInstructions.company_contact_person} />} />
-              <Route path='/date' element={<DateForm selectedDate={selectedDate} setSelectedDate={setSelectedDate} />} />
-              <Route path='/time' element={<TimeForm openingTime={11} closingTime={20} selectedTime={selectedTime} setSelectedTime={setSelectedTime} />} />
-              <Route path='/confirm' element={<Confirmation 
-                selectedType={selectedType}   
-                selectedTime={selectedTime} 
-                peopleAmount={peopleAmount}
-                selectedDate={selectedDate}
-                />}
-              />
-            </Routes>
-          </div>
-        </main>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Routes>
+        <Route path='/book/:restaurantId' element={<Main />} />
+      </Routes>
+    </Router>
+  );
+
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,57 @@
+import japaneseRestaurant from '../japaneseRestaurant.png';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation, useParams} from 'react-router-dom';
+import { Header } from './Header';
+import { NavBar } from './NavBar';
+import { TypeForm } from './typeForm';
+import { PeopleForm } from './peopleForm';
+import { DateForm } from './dateForm';
+import { TimeForm } from './timeForm';
+import { Confirmation } from './confirmation';
+import axios from 'axios';
+
+export const Main = () => {
+    const [selectedType, setSelectedType] = useState('');
+    const [peopleAmount, setPeopleAmount] = useState(0);
+    const [selectedDate, setSelectedDate] = useState(null)
+    const [selectedTime, setSelectedTime] = useState(null)
+    const availableTypes = [
+      'Breakfast',
+      'Lunch',
+      'Dinner'
+    ]
+    const { pathname } = useLocation();
+    const { restaurantId } = useParams();
+    console.log(pathname)
+
+    return (
+        <div>
+        <Header RestaurantName="Sakanaya" />
+        <main style={{display: 'flex', width: '100%', height: '100%'}}>
+          <div className='restaurantImage' style={{ padding: '50px 0px 50px 80px'}}>
+            <img src={japaneseRestaurant} width={450} height={600} alt='Restaurant' />
+          </div>
+          <div className='bookingForm' style={{width: '100%', textAlign: 'center'}}>
+            <NavBar 
+              selectedType={selectedType}
+              peopleAmount={peopleAmount}
+              selectedDate={selectedDate}
+              selectedTime={selectedTime}
+            />
+            {
+                pathname === `${pathname}/type` ? <TypeForm AvailableTypes={availableTypes} setSelectedType={setSelectedType} /> :
+                pathname === `${pathname}/people` ? <PeopleForm peopleAmount={peopleAmount} setPeopleAmount={setPeopleAmount} /> :
+                pathname === `${pathname}/people` ? <DateForm selectedDate={selectedDate} setSelectedDate={setSelectedDate} />:
+                pathname === `${pathname}/people` ? <TimeForm openingTime={11} closingTime={20} selectedTime={selectedTime} setSelectedTime={setSelectedTime} /> :
+                pathname === `${pathname}/people` ? <Confirmation 
+                                                        selectedType={selectedType}   
+                                                        selectedTime={selectedTime} 
+                                                        peopleAmount={peopleAmount}
+                                                        selectedDate={selectedDate}
+                                                    /> : ''
+            }
+          </div>
+        </main>
+      </div>
+    )
+}
