eab46c17032f53fc1fd295ab7e5181df621996e5
Subhojit Saha <subhojitan@gmail.com>
2022-11-16 22:39:57 +0100

    Test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,18 @@ import { Box } from "@mui/system"
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 import { BookingList } from "../components/BookingsList"
+import { useNavigate } from "react-router-dom"
 
 export const Home = (props) => {    
+
+    const redirect = useNavigate();
 
     return (
         <>
             <Header />
             <Box style={{minHeight: '100vh', margin: '0px 20px 0 20px'}} >
                 <BookingList token={props.token} userId={props.userId} />
+                <button onClick={() => redirect('/book/2')} >Visit site</button>
             </Box>
             <Footer />
         </>
