b150228bb7aa1e2e4f0d2383e3e46286d45a9d90
Subhojit Saha <subhojitan@gmail.com>
2023-04-14 19:43:09 +0200

    Changed backend url

--- src/axios.js
+++ src/axios.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export default axios.create({
     // baseURL: "http://localhost:4000/"
     // baseURL: "http://192.168.0.39:4000/"
-    baseURL: "https://reserveit.onrender.com/"
+    // baseURL: "https://reserveit.onrender.com/"
+    baseURL: "https://reserve-it.herokuapp.com/"
 })
