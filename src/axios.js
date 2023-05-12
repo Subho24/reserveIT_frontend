d224129a792272a948345721c10821ad95026cdd
Subhojit Saha <subhojitan@gmail.com>
2023-05-12 18:27:15 +0200

    Fixed time issue on IOS and double booking issue.

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: "http://localhost:4000/"
-    baseURL: "http://192.168.0.32:4000/"
-    // baseURL: "https://reserve-it.herokuapp.com/"
+    // baseURL: "http://192.168.0.32:4000/"
+    baseURL: "https://reserve-it.herokuapp.com/"
 })
